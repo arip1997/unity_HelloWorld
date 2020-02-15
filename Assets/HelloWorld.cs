3c538792b17f6e9fe191cb35ddb84da945860ce4
arip1997 <ariefr.santoso@gmail.com>
2020-02-15 15:20:07 +0700

    view helloworld success

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HelloWorld : MonoBehaviour {
+    public string myText;
     // Start is called before the first frame update
     void Start () {
-        //Debug.Log("Hello World");
+     
     }
 
     // Update is called once per frame
     void Update () {
-        Debug.Log("Hello World");
+    }
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(10, 10, 200, 200));
+        GUILayout.Label(myText);
+        GUILayout.EndArea();
+
     }
 }
